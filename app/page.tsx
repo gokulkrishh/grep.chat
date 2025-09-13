cff1338930d532b4c3551adee7085727d5f44cad
gokulkrishh <2944237+gokulkrishh@users.noreply.github.com>
2025-09-14 00:12:17 +0530

    Remove test placeholder from main section in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex h-full w-full flex-col overflow-hidden">
       <Header />
-      <main className="mx-auto flex h-dvh w-full flex-col items-center justify-center">test</main>
+      <main className="mx-auto flex h-dvh w-full flex-col items-center justify-center"></main>
     </div>
   )
 }
